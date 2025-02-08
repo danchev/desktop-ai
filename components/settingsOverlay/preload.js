0d0f301bf29a45a2ba9f8b57788f5e6bb9cfe87e
D. Danchev <12420863+danchev@users.noreply.github.com>
2025-02-08 12:03:02 -0600

    refactor: improve parameter naming in localStorage methods in settingsOverlay

--- components/settingsOverlay/preload.js
+++ components/settingsOverlay/preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
-  getLocalStorage: (a) => ipcRenderer.invoke("get-local-storage", a),
-  setLocalStorage: (a, b) => ipcRenderer.send("set-local-storage", a, b),
+  getLocalStorage: (key) => ipcRenderer.invoke("get-local-storage", key),
+  setLocalStorage: (key, value) => ipcRenderer.send("set-local-storage", key, value),
   updateWebviewUrl: (url) => ipcRenderer.send("update-webview-url", url),
   close: () => ipcRenderer.send("close"),
 });
