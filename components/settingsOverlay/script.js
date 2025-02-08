187745c175e240a0b3a7bb5887903065164f6f2e
D. Danchev <12420863+danchev@users.noreply.github.com>
2025-02-08 12:04:40 -0600

    refactor: streamline event handling for shortcut registration in settingsOverlay

--- components/settingsOverlay/script.js
+++ components/settingsOverlay/script.js
@@ -2,21 +2,26 @@ const shortcutObjs = document.querySelectorAll(".btn");
 const webviewUrlInput = document.querySelector(".url-input");
 
 const register = (btn) => {
-  let shortcut = [];
+  const shortcut = [];
 
-  document.addEventListener("keydown", (e) => shortcut.push(e.key));
-  document.addEventListener(
-    "keyup",
-    (e) => {
-      if (e.key !== "Backspace") {
-        btn.target.innerText = format(shortcut.splice(0, 3));
-      } else btn.target.innerText = "";
-      shortcut.length = 0;
-    },
-    { once: true },
-  );
+  const keydownHandler = (e) => {
+    shortcut.push(e.key);
+  };
+
+  const keyupHandler = (e) => {
+    document.removeEventListener("keydown", keydownHandler);
+    document.removeEventListener("keyup", keyupHandler);
+
+    if (e.key !== "Backspace") {
+      btn.target.innerText = format(shortcut.splice(0, 3));
+    } else {
+      btn.target.innerText = "";
+    }
+  };
+
+  document.addEventListener("keydown", keydownHandler);
+  document.addEventListener("keyup", keyupHandler, { once: true });
 };
-
 
 function format(array) {
   return array.join(" + ").toLowerCase();
@@ -35,15 +40,21 @@ async function main() {
 
   webviewUrlInput.placeholder = "Enter webview URL";
   webviewUrlInput.value = await window.electron.getLocalStorage("webviewUrl");
-  webviewUrlInput.onfocus = () => { webviewUrlInput.select(); };
-  webviewUrlInput.onkeydown = (e) => { if (e.key === "Enter") document.querySelector(".done").click(); };
+  webviewUrlInput.onfocus = () => {
+    webviewUrlInput.select();
+  };
+  webviewUrlInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      document.querySelector(".done").click();
+    }
+  });
 
-  document.querySelector(".done").onclick = () => {
-    window.electron.setLocalStorage(
+  document.querySelector(".done").onclick = async () => {
+    await window.electron.setLocalStorage(
       "toggleVisibilityShortcut",
       shortcutObjs[0].innerText,
     );
-    window.electron.setLocalStorage("webviewUrl", webviewUrlInput.value);
+    await window.electron.setLocalStorage("webviewUrl", webviewUrlInput.value);
     window.electron.updateWebviewUrl(webviewUrlInput.value);
     window.electron.close();
   };
